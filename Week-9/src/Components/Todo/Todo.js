f91b40c592e6d9242cb63a332be24fff0c51be62
nutangaikwad50 <randiveomkar95@gmail.com>
2022-11-10 08:28:50 +0530

    project moved to particular week

--- Week-9/src/Components/Todo/Todo.js
+++ Week-9/src/Components/Todo/Todo.js
@@ -1,31 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import './todo.css';
 
 const Todo = () => {
   const [task, setTask] = useState("");
   const [tasks, setTasks] = useState([]);
-  useEffect(()=>{
-      getList()
-  },[])
-  const getList = () => {
-      if(localStorage.getItem("tasks")){
-          const storedList = JSON.parse(localStorage.getItem("tasks"));
-          setTasks(storedList);
-      }
-  }
+
   const addTask = (e) => {
       if (task) {
           const newTask = { id: new Date().getTime().toString(), title: task, status: 0 };
           setTasks([...tasks, newTask]);
-          localStorage.setItem("tasks", JSON.stringify([...tasks, newTask]));
           setTask("");
       }
   };
-  const completeTask = (id) => {
-      const storedList = JSON.parse(localStorage.getItem("tasks"));
-        storedList[id].status = 1
-        localStorage.setItem("tasks", JSON.stringify([...storedList]));
-        getList()
+  const completeTask = (index) => {
+      const storedList = [...tasks];
+        storedList[index].status = 1
+        setTasks([...storedList])
   }; 
   return (
     <div className="col-md-12">
@@ -33,6 +23,7 @@ const Todo = () => {
         <div className="col-md-6 center">
         <input
           type="text"
+          value={task}
           className="form-control"
           placeholder="Add task..."
           onChange={(e) => setTask(e.target.value)}
@@ -42,7 +33,7 @@ const Todo = () => {
         <br />
         <div className="col-md-12 row">
         {tasks.map((task,i) => (
-        <div key={task.id}>
+        <div key={task.id} className="col-md-6" style={{gridColumn:'auto'}}>
             <div className={task.status === 1 ? 'complete' : 'card'} style={{"marginLeft" : "10px", "width" : "12rem"}}>
                 <span className={task.status === 1 ? 'task-complete' : ''}>{task.title} </span><br />
                 <button className="btn-success" onClick={() =>completeTask(i)}>{task.status === 1 ? 'Completed' : 'Mark as Complete'} </button>
