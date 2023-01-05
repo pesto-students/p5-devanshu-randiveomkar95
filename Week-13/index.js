e95b59aa0bbcfa8255de6da178f6bd7cfd646094
Omkar Randive <randiveomkar95@gmail.com>
2023-01-05 08:37:42 +0530

    Week 13 assignment complete

--- Week-13/index.js
+++ Week-13/index.js
@@ -1,15 +1,36 @@
-const axios = require("axios");
 const express = require('express');
-
+const axios = require("axios").create({baseUrl: "https://api.openweathermap.org/data/2.5/weather"});
+const Data  = require('./constants')
+const {API_KEY}  = Data
 const app = express();
+app.use(express.json());
 
-async function getUser() {
-  try {
-    const response = await axios.get('http://api.weatherapi.com/v1/current.json?key=e62589f5aaf9464e907170956220512&q=Pune&aqi=no');
-    console.log(response);
-  } catch (error) {
-    console.error(error);
-  }
-}
+    app.get("/", (req, res) => {
+        axios({
+            url: `https://api.openweathermap.org/data/2.5/weather?q=pune&appid=${API_KEY}`,
+            method: "get",
+        })
+        .then(response => {
+            res.status(200).json(response.data);
+        })
+        .catch((err) => {
+            res.status(500).json({ message: err });
+        });
+    });
 
-console.log(getUser());
+    app.post("/search/by/city",(req, res) => {
+        let city = req.body.city;
+        axios({
+            url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`,
+            method: "post",
+        })
+        .then(response => {
+            res.status(200).json(response.data);
+        })
+        .catch((err) => {
+            res.status(500).json({ message: err });
+        });
+    });
+
+
+app.listen(3000, ()=> console.log("running on port 3000"))
